a8aea7ec2334aabd91a1c5d58389b79ef7dae548
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-23 20:43:51 +0330

    develope last payload senario with useSeletor and useDispatch hooks

--- src/Components/CakePayloadContainer.jsx
+++ src/Components/CakePayloadContainer.jsx
@@ -1,28 +1,34 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { connect } from "react-redux";
 import { buyCake } from "./Redux/cake/cakeActions";
 
-const CakePayloadContainer = ({numOfCake, buyCake}) => {
+const CakePayloadContainer = (props) => {
     const [number, setNumber] = useState(1);
+    const numOfCake = useSelector(({cakes}) => cakes.numOfCake);
+    const dispatch = useDispatch();
     return ( 
         <div>
             <h1>Number of the cake - {numOfCake}</h1>
             <input type="text" value={number} onChange={({target}) => setNumber(target.value)} />
-            <button type="button" onClick={()=> buyCake(number || 1)}>buy {number} cake</button>
+            <button type="button" onClick={()=> dispatch(buyCake(number || 1))}>buy {number} cake</button>
         </div>
      );
 }
+
+export default CakePayloadContainer;
  
-const mapStateToProps = (state) =>{
-    return {
-        numOfCake: state.cakes.numOfCake
-    }
-}
+// const mapStateToProps = (state) =>{
+//     return {
+//         numOfCake: state.cakes.numOfCake
+//     }
+// }
 
-const mapDispatchToProps = (dispatch) =>{
-    return {
-        buyCake: number => dispatch(buyCake(number))
-    }
-}
+// const mapDispatchToProps = (dispatch) =>{
+//     return {
+//         buyCake: number => dispatch(buyCake(number))
+//     }
+// }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CakePayloadContainer);
+// export default connect(mapStateToProps, mapDispatchToProps)(CakePayloadContainer);
