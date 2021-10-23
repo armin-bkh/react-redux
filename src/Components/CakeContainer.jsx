ef43232b5d481bcc8e0f0c7a1eee60ca8e1c91a8
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-23 18:15:26 +0330

    use connect higher order component from react-redux package and get state from props with mapStateToProps and mapDispatchToProps functions

--- src/Components/CakeContainer.jsx
+++ src/Components/CakeContainer.jsx
@@ -1,10 +1,23 @@
-const CakeContainer = () => {
-    return ( 
-        <div>
-            <h1>Number of cakes - </h1>
-            <button>Buy cake</button>
-        </div>
-     );
-}
- 
-export default CakeContainer;
+import { connect } from "react-redux";
+import { buyCake } from "./Redux/cake/cakeActions";
+
+const CakeContainer = (props) => {
+  return (
+    <div>
+      <h1>Number of cakes - {props.numOfCake}</h1>
+      <button type="button" onClick={props.buyCake}>
+        Buy cake
+      </button>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return { numOfCake: state.numOfCake };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return { buyCake: () => dispatch(buyCake()) };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer);
