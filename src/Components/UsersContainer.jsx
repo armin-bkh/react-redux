a0dbea04afe26001fd8d8f5086f6a56786028f7f
armin bakhshi <arminbkh0921@yahoo.com>
2021-10-24 00:16:29 +0330

    do dispatch and get users with useSelector and useDispatch hooks

--- src/Components/UsersContainer.jsx
+++ src/Components/UsersContainer.jsx
@@ -1,20 +1,35 @@
 import { useEffect } from "react";
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import { FetchUsers } from "./Redux";
 
-const UsersContainer = ({ users, fetchdata }) => {
-    
-    useEffect(()=>{
-        fetchdata();
-    }, [])
-  return users.loading ? <h1>Loading...</h1> : users.error ? <h1>error accourd</h1> : users.users.map(user => <h1>{user.name}</h1>);
+const UsersContainer = (props) => {
+  const users = useSelector(({ users }) => users);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(FetchUsers());
+  }, []);
+  return users.loading ? (
+    <h1>Loading...</h1>
+  ) : users.error ? (
+    <h1>{users.error.message}</h1>
+  ) : (
+    <div>
+      <h1>user list</h1>
+      {users.users.map((user) => (
+        <h1 key={user.id}>{user.name}</h1>
+      ))}
+    </div>
+  );
 };
 
-const mapStateToProps = (state) => {
-  return { users: state.users };
-};
-const mapDispatchToProps = (dispatch) => {
-  return { fetchdata: () => dispatch(FetchUsers()) };
-};
+export default UsersContainer;
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
+// const mapStateToProps = (state) => {
+//   return { users: state.users };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return { fetchdata: () => dispatch(FetchUsers()) };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
